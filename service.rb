eedd9f46165ba7b8905c269e16271eea61d49073
trond <trond@lofotr.net>
2013-01-23 14:29:51 +0100

    The serviceholder should be complete

--- service.rb
+++ service.rb
@@ -1,10 +1,19 @@
 require 'singleton'
 class Service
   include Singleton
-  @serviceObjects = Hash.new()
+  @serviceObjects
   attr_accessor :serviceObjects
-  def method_missing(m, *args,&block)
+
+  def initialize
+    @serviceObjects = Hash.new()
+  end
+
+  def method_missing(m, *args, &block)
     return @serviceObjects[m]
+  end
+ 
+  def addService(name, service)
+    hash.store(name, service)
   end
 end
 
